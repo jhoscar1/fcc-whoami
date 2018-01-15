107355093c2ee3fd572a4f33af200c2337dfbb42
Jason Oscar <Jason_Oscar@comcast.com>
2018-01-15 13:03:06 -0500

    Initial commit for whoami api for freecodecamp.

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/whoami', (req, res) => {
+  const OsInParens = req.headers['user-agent'].split(')')[0];
+  const OS = OsInParens.slice(OsInParens.indexOf('(') + 1);
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'].split(',')[0],
+    language: req.headers['accept-language'].split(',')[0],
+    software: OS
+  });
+})
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
